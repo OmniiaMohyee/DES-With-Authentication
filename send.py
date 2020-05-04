939813037b9075a3f33fc8d535867acafdb21123
OmniiaMohyee <zicomohyee@yahoo.com>
2020-05-05 01:58:58 +0200

    sender and receiver working with encryption

--- send.py
+++ send.py
@@ -6,22 +6,14 @@ from Crypto.Cipher import DES
 def ECB(message,block_size,key):
     #each character is a byte in our case!
     key = bytes(key,'utf-8')
-    print(len(message))
-    message += ' '
-    print(len(message))
     message = bytes(message,'utf-8')
-    print(len(key))
     cipher = DES.new(key, DES.MODE_ECB)
     msg = cipher.encrypt(message)
     return msg
 
 def CBC(message,block_size,key):
     key = bytes(key,'utf-8')
-    print(len(message))
-    message += ' '
-    print(len(message))
     message = bytes(message,'utf-8')
-    print(len(key))
     cipher = DES.new(key, DES.MODE_CBC)
     msg = cipher.encrypt(message)
     return msg
@@ -29,58 +21,56 @@ def CBC(message,block_size,key):
 
 def CFB(message,block_size,key):
     key = bytes(key,'utf-8')
-    print(len(message))
-    message += ' '
-    print(len(message))
     message = bytes(message,'utf-8')
-    print(len(key))
     cipher = DES.new(key, DES.MODE_CFB)
     msg = cipher.encrypt(message)
     return msg
 
 def CTR(message,block_size,key):
     key = bytes(key,'utf-8')
-    print(len(message))
-    message += ' '
-    print(len(message))
     message = bytes(message,'utf-8')
-    print(len(key))
     cipher = DES.new(key, DES.MODE_CTR)
     msg = cipher.encrypt(message)
     return msg
+def create_tmp_messages(message):
+    l = []
+    for i in range(1,11):
+        l.append(message*i)
+    return l
+
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+
 print("Choose the mode of operation: ECB, CBC, CFB, CTR")
 mode = input()
 print(mode)
 
-# alive = socket.recv()
-# print("Received Alive Message: %s" % alive)
-
-# time.sleep(1)
-# socket.send(bytes(mode, 'utf-8'))
-
 key = "the key!"
-message = "hello, this is a dummy message to test with the different message of operation!"
-encrypted_message = ""
-block_size = 16
-if(mode == "ECB"):
-    encrypted_message = ECB(message,block_size,key)
-elif(mode == "CBC"):
-    encrypted_message = ECB(message,block_size,key)
-elif(mode == "CFB"):
-    encrypted_message = ECB(message,block_size,key)
-elif(mode == "CTR"):
-    encrypted_message = ECB(message,block_size,key)
+message = "hello there you "
+block_size = 8
+messages = create_tmp_messages(message)
+i = 0
 
-print(encrypted_message)
-
-
-
-
-
-
-    
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print("Received request ",message)
+    #  Do some 'work'
+    time.sleep(1)
+    if(i == 0):
+        socket.send(bytes(mode,'utf-8'))
+        i += 1
+    else:
+        if(mode == "ECB"):
+            encrypted_message = ECB(messages[i-1],block_size,key)
+        elif(mode == "CBC"):
+            encrypted_message = ECB(messages[i-1],block_size,key)
+        elif(mode == "CFB"):
+            encrypted_message = ECB(messages[i-1],block_size,key)
+        elif(mode == "CTR"):
+            encrypted_message = ECB(messages[i-1],block_size,key)
+        socket.send(encrypted_message)
+        i += 1
